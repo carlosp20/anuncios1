5af9e0b243ecb126fd10edf3997ff7979ea0922a
unknown <carlosp.07btl@gmail.com>
2014-03-12 08:04:45 -0600

    Primer comit

---
+++ App_Code/Orden.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Orden
+/// </summary>
+public class Orden
+{
+    public int id;
+    public DateTime fechadeentrega;
+    public string name = "";
+    public string sku = "";
+    public int cantidad = 0;
+    
+	public Orden()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
